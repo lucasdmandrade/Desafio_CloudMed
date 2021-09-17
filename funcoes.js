a24816899d3ef0534de599f960d7c635807d521d
lucasdmandrade <lucasdmandrade@gmail.com>
2021-09-17 20:04:18 -0300

    Listagem1 livros funcional

--- funcoes.js
+++ funcoes.js
@@ -1,37 +1,45 @@
-function listagemLivros(){
+function listagemLivros() {
     let livros = []
-livros[0] = []
-livros[1] = []
-livros[2] = []
-livros[3] = ['Fumaça e espelhos', 1998, 4.2, 1500,['Contos', 'Ficção'],'']
+    livros[0] = ['Neve vidro e maçãs', 2019, 4.2, 1500, ['Contos', 'Ficção'], '', '"imagens/neve vidro e maçãs - 2019.png"']
+    livros[1] = ['Mitologia Nórodica', 2017, 4.2, 1500, ['Contos', 'Ficção'], '', '"imagens/mitologia nordica  - 2017.png"']
+    livros[2] = ['Criaturas estranas', 2013, 4.2, 1500, ['Contos', 'Ficção'], '', '"imagens/Criaturas estranhas - 2013.png"']
+    livros[3] = ['Fumaça e espelhos', 1998, 4.2, 1500, ['Contos', 'Ficção'], '', '"imagens/Fumaça e espelhos - 1999.png"']
+    livros[4] = ['Sandman - Fim dos mundos', 1994, 4.2, 1500, ['Contos', 'Ficção'], '','"imagens/Sandman - Fim dos Mundos 1994.png"']
+    livros[5] = ['Lugar nenhum', 1996, 4.2, 1500, ['Contos', 'Ficção'], '', '"imagens/Lugar nenhum - 1996.png"']
 
-    console.log(livros[3][0])
-    const divPronta = `
-    <div id="livro" class="livro">
-        <button onclick=""> <img src="imagens/neve vidro e maçãs - 2019.png" /> </button>
-        <h3>${livros[3][0]}</h3>
-        <p>${livros[3][1]}</p>
-    </div>
+    let divPronta = '<div id="listalivros" class="listalivros">'
+
+    for (var i = 0; i < livros.length; i++) {
+        console.log(livros[i])
+        divPronta += `
+        <div class="livro">
+            <button onclick=""> <img src=${livros[i][6]} /> </button>
+            <h3>${livros[i][0].toUpperCase()}</h3>
+            <p>${livros[i][1]}</p>
+        </div>
 `
-console.log(divPronta)
-    const livro = document.querySelector(' .livro')
-    console.log(livro)
-    livro.innerHTML = divPronta
+    }
+    divPronta += '</div>'
+
+    console.log(divPronta)
+    const listalivros = document.querySelector(' .listalivros')
+    console.log(listalivros)
+    listalivros.innerHTML = divPronta
     console.log('teste')
 }
 listagemLivros()
 
 //Função paginação livros
-function pagination(livros){
+function pagination(livros) {
     //Quantidade de paginas
     let contPages = 1
 
     //Definindo quantas paginas existirão
-    if(livros.length % 10 == 0) {
+    if (livros.length % 10 == 0) {
         contPages += livros.length % 10
-    } else{
+    } else {
         let teste = 0
-        teste = livros.length - (livros.lenght % 10) 
+        teste = livros.length - (livros.lenght % 10)
         constPages += teste / 10
     }
 
