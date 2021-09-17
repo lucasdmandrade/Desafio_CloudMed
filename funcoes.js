290623344063a092c44f570e84060683c9eabea7
lucasdmandrade <lucasdmandrade@gmail.com>
2021-09-17 15:58:35 +0000

    esboço paginação

--- funcoes.js
+++ funcoes.js
@@ -7,3 +7,21 @@ livros[3] = {
     estilo: ['Contos', 'Ficção'],
     resenha: ''
 }
+
+//Função paginação livros
+function pagination(livros){
+    //Quantidade de paginas
+    let contPages = 1
+
+    //Definindo quantas paginas existirão
+    if(livros.length % 10 == 0) {
+        contPages += livros.length % 10
+    } else{
+        let teste = 0
+        teste = livros.length - (livros.lenght % 10) 
+        constPages += teste / 10
+    }
+
+    //Falta puxar os livros de acordo com a pagina
+    // (pagina - 1) * 10 e receber o vetor auto incrementando ate 9
+}
