d67cf140c669d369997c66274d9ef1975b96bc4b
lucasdmandrade <lucasdmandrade@gmail.com>
2021-09-21 20:50:54 -0300

    Melhorando nomes das classes

--- funcoes.js
+++ funcoes.js
@@ -1,7 +1,7 @@
-//Contiuação do texto sobre o autor
+//Contiuação do texto sobre o autor que sera exibido ao clicar no botão "ver mais"
 let textoCompleto = "Hoje vive perto de Minneapolis, nos Estados Unidos. Descobriu seu amor pelos livros na infância e devorava as histórias de C.S. Lewis, J.R.R. Tolkien, James Branch Cabell e Edgar Alan Poe, entre outros autores. Começou a carreira como jornalista, mas logo o talento para construir tramas e universos únicos o levou para o mundo dos quadrinhos, com a aclamada série Sandman, e depois para a ficção adulta e infantojuvenil. Suas obras receberam inúmeros prêmios e medalhas e foram adaptadas em bem-sucedidas versões para cinema, televisão e até ópera."
-let state = 1//Numeração das paginas
-let minhaLista = [1, 5]//Lista de livros do usuario
+let state = 1 //Numeração das paginas(Define a pagina atual e é passado como id de elementos que a usam )
+let minhaLista = [1, 5]//Lista de livros do usuario(por estar definida aqui reseta quando o usuario voltar ao index)
 let livros = []//Vetor com os livros
 livros[0] = ['Neve vidro e maçãs', 2019, 4.2, 1500, 'Contos - Ficção', '', '"imagens/neve vidro e maçãs - 2019.png"']
 livros[1] = ['Mitologia Nórodica', 2017, 3.7, 1500, 'Contos - Ficção', '', '"imagens/mitologia nordica  - 2017.png"']
@@ -31,7 +31,7 @@ livros[24] = ['Sandman Versão Definitiva (volume 1)', 1996, 3.1, 1500, 'Contos 
 livros[25] = ['Coraline', 2002, 4, 1500, 'Contos - Ficção', '', '"imagens/Coraline - 2002.png"']
 
 //
-//FUNÇÃO PARA CRIAR LISTA COM OS LIVROS DA PAGINA
+//FUNÇÃO PARA CRIAR LISTA COM OS LIVROS DA PAGINA(E os botões da paginação)
 //
 function listagemLivros(state, livros) {
 
@@ -44,13 +44,17 @@ function listagemLivros(state, livros) {
         window.alert('Impossivel(err: não há mais livros)')
     } else {
 
-        // Loop para crir divs para cada livro
+        //logica para paginação 
+        // Loop para criar as divs de cada livro(com a classe livro + classe de para criar transição)
         //Usando state para paginação 
+        //Primeira fileira
         for (var i = (state - 1) * 12; i < ((state - 1) * 12 + 6); i++) {
             console.log(livros[i])
             //Só cria a div para o livro caso exista informação pra ser inserida(Se tem mais livros)
             if (livros[i] != null) {
-                //href para inserir id da imagem q quero aumentar na url 
+                //href para inserir id da imagem na url 
+                //Precisa para criar animação, porem muda centralidade da pagina
+                //"Resolvido" redefindo a posição para a inicial após chamar a função
                 divPronta += `
             <div class="livro livroTransition">
                 <a id=${i} href="#${i}" onClick="selecaoLivro(this.id, livros)">
@@ -63,6 +67,8 @@ function listagemLivros(state, livros) {
         } divPronta += `</div>
     <div class="listalivros2">
     `
+        //Logica da paginação 
+        //Na pagina ficaram os livros de 12 em 12, porem divididos em 2 fileiras
         for (var i = ((state - 1) * 12) + 6; i < ((state) * 12); i++) {
             console.log(livros[i])
             //Só cria a div para o livro caso exista informação pra ser inserida(Se tem mais livros)
@@ -87,9 +93,20 @@ function listagemLivros(state, livros) {
                     </button>
                     <div class="numeroPag">
                     `
-        for (let j = state - 2; j <= 5; j++) {
+
+        //Numeros mostrados na paginação 
+        //Na imagem a ser replicada apareciam as paginas: 1, 2, 3, 4, 5 ... 10)
+        //Como na imagem dava para ver que as paginas não variavam de acordo com ter infos nela ou não
+        //Essa foi a logica que eu imaginei que a pagina adotaria para não mostrar as paginas chumbadas
+
+        //Na imagem estava na pagina 1 mostrava ate a 5 
+        //Imaginei que mostraria sempre 5 a frente
+        //Chutei que tambem mostraria 2 atras 
+        //E mostrou a 10, imaginei que aparecia 9 a frente da pagina que o usuario estivesse 
+        for (let j = state - 2; j <= state + 4; j++) {
             if (j > 0) {
                 if (j == state) {
+                    //Utilizei a tag hr variando de acordo com a pagina que estivesse imaginando que ela estaria indicando isso
                     divPronta += `
                                 <p class="paginaAtual">${j} <hr/></p>
                                 `
@@ -100,6 +117,7 @@ function listagemLivros(state, livros) {
                 }
             }
         }
+        //3 pontos entre as paginas proximas e a ultima mostrada conforme imagem 
         divPronta += `
         <p>...</p>
         `
@@ -131,7 +149,7 @@ function listagemLivros(state, livros) {
 function selecaoLivro(i, livros) {
 
 
-    //Contador para quantas estrelas o livro recebeu
+    //Variavel para armazenar a quantidade de incones de estrelas de acordo com a nota do livro 
     let qntEstrelas = ''
     //Quantas estrelas inteiras o livro tem 
     for (var j = 1; j < livros[i][2]; j++) {
@@ -144,7 +162,7 @@ function selecaoLivro(i, livros) {
 
     //Se a nota do livro tiver parte decimal...
     if (livros[i][2] % 1 != 0) {
-        //Se a parte decimal for maior do que 0,5 arredonda pra cima
+        //Se a parte decimal for maior do que 0,5 o livro ganha uma estrela completa(arredonda pra cima)
         if (livros[i][2] % 1 > 0.5) {
             qntEstrelas += `
         <img src="icones/star.svg" alt="icone de strela">
@@ -157,7 +175,10 @@ function selecaoLivro(i, livros) {
             console.log(qntEstrelas)
         }
     } else {
-        //Atribuindo a estrela "extra"(Por ter arredondado o valor para cima)
+        //Atribuindo a estrela caso livro não tenha parte decimal
+        //A ultima estrela é atribuida depois para que caso o livro tenha parte decimal ela não seja atribuida 2 vezes
+        //Uma pela parte decimal no for
+        //E outra na validação se a parte decimal é maior que 0.5(Estrela interia), ou menor ou igual(Meia estrela)
         qntEstrelas += `
         <img src="icones/star.svg" alt="icone de strela">
         `
@@ -183,7 +204,7 @@ function selecaoLivro(i, livros) {
                     </p>
                     <h4>${livros[i][4]}</h4>
 
-                    <div class="addlista">
+                    <div class="addLista">
                         <button id="${i}" onclick="inserirLivro(this.id, minhaLista)">Adicionar á minha lista</button>
                     </div>
                 </div>
@@ -203,18 +224,18 @@ function selecaoLivro(i, livros) {
                     <h1>${livros[i][0]} - ${livros[i][1]}</h1>
                 </div>
                 <div>
-                    <div class="areaavaliacao">
+                    <div class="areavaAliacao">
                     `
     //Definindo cor do quadrado de avaliação 
     if (livros[i][2] >= 3.5) {
         paginaCompleta = paginaCompleta + `
-                        <div class="quadradonota verde">${livros[i][2]}</div>
+                        <div class="quadradoNota verde">${livros[i][2]}</div>
                         `} else if (livros[i][2] > 1.5 && livros[i][2] < 3.5) {
         paginaCompleta = paginaCompleta + `
-                            <div class="quadradonota amarelo">${livros[i][2]}</div>
+                            <div class="quadradoNota amarelo">${livros[i][2]}</div>
                         `} else {
         paginaCompleta = paginaCompleta + `
-                            <div class="quadradonota vermelho">${livros[i][2]}</div>
+                            <div class="quadradoNota vermelho">${livros[i][2]}</div>
                         `}
 
     paginaCompleta = paginaCompleta + `
@@ -264,9 +285,11 @@ function criarPagina(paginaMontada) {
 //Função para IR PARA PROXIMA PAGINA(OBRAS)
 //
 function proxPagina(state) {
+    //Aumenta valor do estado da pagina
     state++
     console.log(state)
 
+    //Chama função que que gera listagem 
     listagemLivros(state, livros)
 }
 
@@ -274,13 +297,15 @@ function proxPagina(state) {
 //FUNÇÃO PARA VOLTAR PAGINA(OBRAS)
 //
 function antPagina(state) {
+    //Confere se é possivel voltar
     if (state > 1) {
         state = state - 1
-    } else {
+    } else {//Se não for mostra erro
         window.alert('Não há pagina anteriorior')
     }
     console.log(state)
 
+    //Chama função que gera listagem
     listagemLivros(state, livros)
 
     return state
@@ -295,14 +320,14 @@ function inserirLivro(i, minhaLista) {
 
     //Teste se livro existe na lista
     for (let j = 0; j < minhaLista.length; j++) {
-        if (minhaLista[j] == parseInt(i)) {
+        if (minhaLista[j] == parseInt(i)) {//Se existir atualiza valor da variavel como true
             livroJaAdd = true
         }
     }
-    //Se ele existeir avisa caso contrario adiciona
+    //Se ele existir avisa 
     if (livroJaAdd == true) {
         window.alert('Livro ja existe em sua lista')
-    } else {
+    } else {//Se não adiciona e avisa que o livro foi adicionado
         minhaLista.push(parseInt(i))
         window.alert('Livro adicionado a sua lista')
     }
@@ -317,17 +342,20 @@ function expandirTexto(textoCompleto) {
     //Inserindo conteudo no paragrafo "continuacao"
     const continuacao = document.querySelector('.continuacao')
     console.log(continuacao)
+    //Nova parte do texto que vai ser inserida
     let texto = textoCompleto
+    //Inserindo 
     continuacao.innerHTML = texto
     console.log(continuacao)
 
-    //Transformando botão em ver menos
+    //Buscando botão ver mais para transforma-lo em ver menos
     const buttons = document.querySelector('.buttons')
 
+    //Inserindo botão novo
     buttons.innerHTML = `
         <div class="buttons">
             <button class="seguir" onclick="">Seguir</button>
-            <button class="buttonpadrao" onclick="reduzirTexto()">Ver menos</button>
+            <button class="buttonPadrao" onclick="reduzirTexto()">Ver menos</button>
         </div>
     `
     console.log(buttons)
@@ -336,16 +364,19 @@ function expandirTexto(textoCompleto) {
 //FUNÇÃO PARA REDUZIR TESTO DO AUTOR
 //
 function reduzirTexto() {
+    //Recebendo texto aumentado
     const continuacao = document.querySelector('.continuacao')
+    //Inserindo vazio para ele sumir
     continuacao.innerHTML = ''
 
-    //Voltando botão ver mais
+    //Buscando botão "ver menos" para voltar a ser "ver mais"
     const buttons = document.querySelector('.buttons')
 
+    //Passando conteudo como botão "ver mais"
     buttons.innerHTML = `
         <div class="buttons">
             <button class="seguir" onclick="">Seguir</button>
-            <button class="buttonpadrao" onclick="expandirTexto(textoCompleto)">Ver mais</button>
+            <button class="buttonPadrao" onclick="expandirTexto(textoCompleto)">Ver mais</button>
         </div>
     `
 }
